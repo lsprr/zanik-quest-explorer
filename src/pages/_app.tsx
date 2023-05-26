36a5e61fc776f1c3542adfb3e0bf15ba289777f2
lsprr <16653744+lsprr@users.noreply.github.com>
2023-05-26 17:03:10 -0400

    Update meta description tag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,7 +10,7 @@ export default function App({ Component, pageProps }: AppProps) {
             <Head>
                 <title>Zanik Quest Explorer</title>
                 <meta name="description"
-                      content="A comprehensive, user-friendly guide to OSRS Quests. Traverse quests, skills, and mysteries of Gielinor with ease. Built with Next.js, React and Vanilla CSS. Join us as we delve into the thrilling realms of Old School Runescape together!" />
+                      content="Unleash your Old School RuneScape (OSRS) questing potential with Zanik Quest Explorer. Get detailed quest info, exhaustive quest lists, and player stats assessments at your fingertips. Enhance your OSRS journey today!" />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
                 <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
