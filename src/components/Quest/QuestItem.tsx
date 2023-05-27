ff3f25867967f1dd144e81fae7cd038b82c7892a
lsprr <16653744+lsprr@users.noreply.github.com>
2023-05-26 20:42:37 -0400

    Minor corrections

--- src/components/Quest/QuestItem.tsx
+++ src/components/Quest/QuestItem.tsx
@@ -1,5 +1,7 @@
 import { Quest } from "@/types/quests.interface";
 import styles from './QuestItem.module.css';
+import Image from "next/image";
+import questsImage from 'public/assets/image/quests.png';
 
 interface QuestItemProps {
     quest: Quest;
@@ -12,12 +14,15 @@ export const QuestItem = ({ quest, isQualified, handleOpenModal }: QuestItemProp
         <div
             className={styles.questItem}
             onClick={() => handleOpenModal(quest)}
-            onKeyDown={(e) => { if(e.key === 'Enter' || e.key === ' ') handleOpenModal(quest) }}
+            onKeyDown={(e) => {
+                if (e.key === 'Enter' || e.key === ' ') handleOpenModal(quest)
+            }}
             role="button"
             tabIndex={0}
             aria-label={`Quest item: ${quest.title}. Status: ${isQualified ? 'qualified' : 'unqualified'}`}
         >
             <h2 className={`${styles.questItem__title} ${isQualified ? styles['questItem__status-qualified'] : styles['questItem__status-unqualified']}`}>
+                <span className={styles.questItem__icon}><Image src={questsImage} alt='quests icon' /></span>
                 {quest.title}
             </h2>
         </div>
