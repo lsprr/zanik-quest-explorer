2e5ea3d64dfd2f3147f231ec6d8b96d24de92dca
lsprr <16653744+lsprr@users.noreply.github.com>
2023-05-26 23:26:29 -0400

    Add logic to classify quests based on skill requirements

--- src/components/Quest/QuestList.tsx
+++ src/components/Quest/QuestList.tsx
@@ -26,23 +26,54 @@ export const QuestList = ({ skills, quests, loading }: QuestListProps) => {
         setSelectedQuest(null);
     };
 
+    const isQuestQualified = (quest: Quest, skills: Partial<Skills>): boolean => {
+        // The function uses Array.prototype.every() on quest['required-skills'] array.
+        // .every() checks if all elements in an array pass a test (provided as a function).
+        return quest['required-skills'].every(skillRequirement => {
+            // Here we use a regular expression to match the level and skill name from the skillRequirement string.
+            const levelMatch = skillRequirement.match(/(\d+)\s(.+)/i);
+            // If the string doesn't match our regular expression (meaning it's not a skill requirement), we return true.
+            // This means for the purpose of our .every() check, this requirement is considered passed.
+            if (!levelMatch) {
+                return true;
+            }
+            // If we have a match, we parse the required level and the skill name.
+            const requiredLevel = parseInt(levelMatch[1], 10);
+            const skillName = levelMatch[2].toLowerCase().trim();
+
+            // If the player does not have this skill, we return true.
+            // This requirement is considered passed for the purpose of our .every() check.
+            if (!(skillName in skills)) {
+                return true;
+            }
+
+            // If the player does have this skill, we compare the player's level in this skill with the required level.
+            // If the player's level is greater than or equal to the required level, we return true, else we return false.
+            return (skills[skillName as keyof Skills]?.level || 0) >= requiredLevel;
+        });
+    };
+
+    // We use Array.prototype.reduce() on the quests array.
+    // .reduce() executes a reducer function on each element of the array, resulting in a single output value.
     const { qualified, unqualified } = quests.reduce(
         (acc, quest) => {
-            const isQualified = quest["required-skills"].every(skillRequirement => {
-                const [levelString, skillName] = skillRequirement.split(" ");
-                const requiredLevel = parseInt(levelString.replace("Level", "").trim(), 10);
+            // We determine whether the player is qualified for the current quest.
+            const isQualified = isQuestQualified(quest, skills);
 
-                return (skills[skillName as keyof Skills]?.level || 0) >= requiredLevel;
-            });
-
+            // If the player is qualified, we add the quest to the 'qualified' array.
+            // If not, we add it to the 'unqualified' array.
             if (isQualified) {
                 acc.qualified.push(quest);
             } else {
                 acc.unqualified.push(quest);
             }
 
+            // We return our accumulator object, which keeps track of our 'qualified' and 'unqualified' arrays.
+            // This will be passed as the 'acc' argument in the next call of the reducer function.
             return acc;
         },
+        // The second argument to .reduce() is the initial value of our accumulator.
+        // In our case, it's an object with two empty arrays 'qualified' and 'unqualified'.
         { qualified: [] as Quest[], unqualified: [] as Quest[] }
     );
 
@@ -52,7 +83,8 @@ export const QuestList = ({ skills, quests, loading }: QuestListProps) => {
             <div className={styles.quests__body}>
                 {loading ? <Loading className={styles.quests__bodyLoading} color={"black"} /> :
                     [...qualified, ...unqualified].map(quest =>
-                        <QuestItem key={quest.id} quest={quest} isQualified={qualified.includes(quest)} handleOpenModal={handleOpenModal} />
+                        <QuestItem key={quest.id} quest={quest} isQualified={qualified.includes(quest)}
+                                   handleOpenModal={handleOpenModal} />
                     )
                 }
             </div>
